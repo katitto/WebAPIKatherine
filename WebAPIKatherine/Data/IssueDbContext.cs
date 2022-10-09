023c126220e0a33ac8a41f783aea01799fd99fe5
b-katherine.almache <b-katherine.almache@B-KATHERINEALMA>
2022-10-09 22:35:22 +0200

    Add project files.

---
+++ WebAPIKatherine/Data/IssueDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIKatherine.Models;
+
+namespace WebAPIKatherine.Data
+{
+    public class IssueDbContext: DbContext 
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options)
+            : base(options)
+        { }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ WebAPIKatherine/Models/Issue.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIKatherine.Models
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public IssueType IssueType { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Completed { get; set; }
+    }
+
+    public enum Priority 
+    { 
+        low, medium, High
+    }
+
+    public enum IssueType
+    { feature, Bug, Documentation
+    }
+}
